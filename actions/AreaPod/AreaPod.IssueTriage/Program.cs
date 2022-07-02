3c3f0d321ba330ad55fcff23e6bd663879426006
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-07-01 17:41:10 -0700

    Stub out the AreaPod.IssueTriage action as a Hello World console app

---
+++ actions/AreaPod/AreaPod.IssueTriage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AreaPod.IssueTriage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
