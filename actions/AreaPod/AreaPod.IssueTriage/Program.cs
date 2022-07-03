141f50f508d0bf2e2c6270ca459c0da0b7032c8d
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-07-03 12:26:52 -0700

    Detect GITHUB_TOKEN from action app

--- actions/AreaPod/AreaPod.IssueTriage/Program.cs
+++ actions/AreaPod/AreaPod.IssueTriage/Program.cs
@@ -7,6 +7,17 @@ namespace AreaPod.IssueTriage
         static void Main(string[] args)
         {
             Console.WriteLine($"Triaging Issue for Pod: {string.Join(' ', args)}");
+
+            string? GITHUB_TOKEN = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+
+            if (GITHUB_TOKEN is null)
+            {
+                Console.WriteLine("No GITHUB_TOKEN available");
+            }
+            else
+            {
+                Console.WriteLine("GITHUB_TOKEN available");
+            }
         }
     }
 }
