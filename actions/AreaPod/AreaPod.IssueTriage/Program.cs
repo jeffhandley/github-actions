ae6644a3d07ffbddf43a2258af0fc5250c927fcf
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-07-02 23:25:57 -0700

    Add areapod-issuetriage to PATH via /actions

--- actions/AreaPod/AreaPod.IssueTriage/Program.cs
+++ actions/AreaPod/AreaPod.IssueTriage/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace AreaPod.IssueTriage
+﻿using System;
+
+namespace AreaPod.IssueTriage
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Hello, World! {string.Join(' ', args)}");
         }
     }
 }
